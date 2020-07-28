2343b57feb04ec3c3ee2f506d0cbe29a6b94edb4
natuparth <parth.natu01@gmail.com>
2020-07-28 13:05:02 +0530

    first commit

---
+++ src/components/app/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../header/Header';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App App-header">
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './header.css';
+
+
+function Header(){
+  return (
+
+  <h2 className="Header-logo"> Welcome to BluffMaster</h2>
+
+  )
+
+}
+
+export default Header;
