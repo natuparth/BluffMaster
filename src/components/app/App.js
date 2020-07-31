67a009db608d6b6b4b47e2ef40cccaf5172743bb
natuparth <parth.natu01@gmail.com>
2020-07-31 13:07:08 +0530

    first commit in develop branch

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Header from '../header/Header';
 import './App.css';
+import Main from '../main/main';
+import Game from '../game/game';
 
 function App() {
   return (
     <div className="App App-header">
       <Header/>
+     {/* <Main/> */}
+     <Game/>
     </div>
   );
 }
